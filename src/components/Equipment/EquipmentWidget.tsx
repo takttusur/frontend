dee36706b27687275d3939b1bfe3f031c855ae47
DVZakharov <131305853+DVZakharov@users.noreply.github.com>
2023-08-28 19:48:59 +0700

    Folders were created for News, Equipment, and Current Events components, as well as components and widgets were created inside them; The react router is installed;

---
+++ src/components/Equipment/EquipmentWidget.tsx
@@ -0,0 +1,15 @@
+import { Text, Box, useColorModeValue, Center } from '@chakra-ui/react';
+
+export default function Equipment() {
+  return (
+    <Box borderWidth='1px' borderColor='black' bg={useColorModeValue('gray.200', 'gray.700')} h={400}>
+        <Center>
+            <Box display='flex' alignItems='center' w='90%' h={398} bg='gray.400'>
+                <Box display="flex" alignItems="center" w='100%'>
+                    <Text w='100%' textAlign='center'> Equipment </Text>
+                </Box>         
+            </Box>
+        </Center>            
+    </Box>
+  );
+}
