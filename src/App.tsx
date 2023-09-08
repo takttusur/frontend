5fbb33746cab98605a468be69556193609875575
DVZakharov <131305853+DVZakharov@users.noreply.github.com>
2023-09-09 01:12:08 +0700

    The router is transferred from the main.tsx in the App.tsx;   Deleted files: News.tsx, CurrentEvents.tsx, Equipment.tsx and error-page.tsx;

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import './App.css';
 import MainMenu from './components/Navigation/MainMenu';
-import WallpaperCarousel from './components/WallpaperCarousel';
-import CurrentEventsWidget from './components/CurrentEvents/CurrentEventsWidget';
-import EquipmentWidget from './components/Equipment/EquipmentWidget';
-import NewsWidget from './components/News/NewsWidget';
 import Footer from './components/Footer';
+import MainPage from './MainPage';
+import CurrentEventsComponent from './components/CurrentEvents/CurrentEventsComponent';
+import EquipmentComponent from './components/Equipment/EquipmentComponent';
+import NewsComponent from './components/News/NewsComponent';
+
+import { Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
         <>
             <MainMenu />
-            <WallpaperCarousel/>
-            <CurrentEventsWidget/>
-            <EquipmentWidget/>
-            <NewsWidget/>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/CurrentEvents" element={<CurrentEventsComponent />} />
+              <Route path="/Equipment" element={<EquipmentComponent />} />
+              <Route path="/News" element={ <NewsComponent /> } />
+            </Routes>
             <Footer/>
         </>
   );
