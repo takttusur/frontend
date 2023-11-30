61a55fa3de605f521d01e101bc3294f77da2a48d
larkenson <faustov.cfif@yandex.ru>
2023-11-30 23:19:22 +0700

    af
    stopSlider

--- src/components/WallpaperCarousel.tsx
+++ src/components/WallpaperCarousel.tsx
@@ -1,34 +1,44 @@
-import { Text, Box, Flex, Image } from '@chakra-ui/react';
+import { Text, Box, Flex, Image} from '@chakra-ui/react';
 import { useEffect, useState } from "react";
 import "./WallpaperCarousel.css";
 
 interface IWallpaperCaruselProps{
   slides:string[]
 }
+interface IWallpaperCaruselState{
+  currentSlide:number,
+  stopSliding:boolean
+}
 
 export default function WallpaperCarousel(props:IWallpaperCaruselProps) {
   
-  const [currentSlide, setCurrentSlide] = useState(0);
+  const [state, setState] = useState<IWallpaperCaruselState>({
+    currentSlide:0,
+    stopSliding:false
+  });
 
   const slidesCount = props.slides.length;
   const carouselStyle = {
     transition: "all .5s",
-    ml: `-${currentSlide * 100}%`,// мах ширина
+    ml: `-${state.currentSlide * 100}%`,// мах ширина
   };
   const SLIDES_INTERVAL_TIME = 3000;// время
   const ANIMATION_DIRECTION = "right";
   useEffect(() => {
     const prevSlide = () => {
-      setCurrentSlide((s) => (s === 0 ? slidesCount - 1 : s - 1));
+      setState((s) => ({...s,currentSlide:s.currentSlide === 0 ? slidesCount - 1 : s.currentSlide - 1 }));
     };
-
+    
     const nextSlide = () => {
-      setCurrentSlide((s) => (s === slidesCount - 1 ? 0 : s + 1));
+      setState((s) => ({...s,currentSlide:s.currentSlide === slidesCount - 1 ?  0 : s.currentSlide + 1}));
     };
 
     const automatedSlide = setInterval(() => {
+      if (state.stopSliding)return;
       ANIMATION_DIRECTION.toLowerCase() === "left" ? prevSlide() : nextSlide();
     }, SLIDES_INTERVAL_TIME);
+
+
     return () => clearInterval(automatedSlide);
   }, [slidesCount]);
   return (
@@ -37,7 +47,14 @@ export default function WallpaperCarousel(props:IWallpaperCaruselProps) {
       alignItems="center"
       justifyContent="center"
       overflow="hidden"
-    >
+      onMouseOver={()=>{
+        setState(s=>({...s,stopSliding:true}))
+      }}
+      onMouseOut={()=>{
+        setState(s=>({...s,stopSliding:false}))
+      }}
+      >
+
       <Flex w="auto">
         <Flex pos="relative" w="full" {...carouselStyle}>
           {props.slides.map((slide, sid) => (
