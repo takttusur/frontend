c66b03436cd903ae9da1989e71ea50ce1cef8083
andreygolubkow <andreygolubkow@gmail.com>
2023-12-09 01:48:03 +0400

    Переформатирован код, добавлена остановка слайдера при наведении курсора, исправлены размеры блока с фото

--- src/components/WallpaperCarousel.tsx
+++ src/components/WallpaperCarousel.tsx
@@ -1,95 +1,102 @@
-import { Text, Box, Flex, Image} from '@chakra-ui/react';
-import { useEffect, useState } from "react";
+import {Text, Box, Flex, Image} from '@chakra-ui/react';
+import {useEffect, useState} from "react";
 import "./WallpaperCarousel.css";
 
-interface IWallpaperCaruselProps{
-  slides:string[]
-}
-interface IWallpaperCaruselState{
-  currentSlide:number,
-  stopSliding:boolean
+interface IWallpaperCaruselProps {
+    slides: string[]
 }
 
-export default function WallpaperCarousel(props:IWallpaperCaruselProps) {
-  
-  const [state, setState] = useState<IWallpaperCaruselState>({
-    currentSlide:0,
-    stopSliding:false
-  });
+interface IWallpaperCaruselState {
+    currentSlide: number,
+    stopSliding: boolean
+}
 
-  const slidesCount = props.slides.length;
-  const carouselStyle = {
-    transition: "all .5s",
-    ml: `-${state.currentSlide * 100}%`,// мах ширина
-  };
-  const SLIDES_INTERVAL_TIME = 3000;// время
-  const ANIMATION_DIRECTION = "right";
-  useEffect(() => {
-    const prevSlide = () => {
-      setState((s) => ({...s,currentSlide:s.currentSlide === 0 ? slidesCount - 1 : s.currentSlide - 1 }));
+export default function WallpaperCarousel(props: IWallpaperCaruselProps) {
+
+    const [state, setState] = useState<IWallpaperCaruselState>({
+        currentSlide: 0,
+        stopSliding: false
+    });
+
+    const slidesCount = props.slides.length;
+    const carouselStyle = {
+        transition: "all .5s",
+        ml: `-${state.currentSlide * 100}%`,// мах ширина
     };
-    
-    const nextSlide = () => {
-      setState((s) => ({...s,currentSlide:s.currentSlide === slidesCount - 1 ?  0 : s.currentSlide + 1}));
-    };
+    const SLIDES_INTERVAL_TIME = 3000;// время
+    const ANIMATION_DIRECTION = "right";
+    useEffect(() => {
+        const prevSlide = () => {
+            setState((s) =>
+                ({
+                    ...s,
+                    currentSlide: s.stopSliding ? s.currentSlide : s.currentSlide === 0 ? slidesCount - 1 : s.currentSlide - 1
+                })
+            );
+        };
 
-    const automatedSlide = setInterval(() => {
-      if (state.stopSliding)return;
-      ANIMATION_DIRECTION.toLowerCase() === "left" ? prevSlide() : nextSlide();
-    }, SLIDES_INTERVAL_TIME);
+        const nextSlide = () => {
+            setState((s) =>
+                ({
+                    ...s,
+                    currentSlide: s.stopSliding ? s.currentSlide : s.currentSlide === slidesCount - 1 ? 0 : s.currentSlide + 1
+                })
+            );
+        };
 
+        const automatedSlide = setInterval(() => {
+            ANIMATION_DIRECTION.toLowerCase() === "left" ? prevSlide() : nextSlide();
+        }, SLIDES_INTERVAL_TIME);
+        return () => clearInterval(automatedSlide);
+    }, [slidesCount]);
 
-    return () => clearInterval(automatedSlide);
-  }, [slidesCount]);
-  return (
-    <Flex
-      w="full"
-      alignItems="center"
-      justifyContent="center"
-      overflow="hidden"
-      onMouseOver={()=>{
-        setState(s=>({...s,stopSliding:true}))
-      }}
-      onMouseOut={()=>{
-        setState(s=>({...s,stopSliding:false}))
-      }}
-      >
-
-      <Flex w="auto">
-        <Flex pos="relative" w="full" {...carouselStyle}>
-          {props.slides.map((slide, sid) => (
-            <Box key={`slide-${sid}`} flex="none" boxSize="full" shadow="md" height={{
-              base: "100%", // 0-48em
-              md: "50%", // 48em-80em,
-              lg: "40%", // 48em-80em,
-               // 80em+
+    return (
+        <Flex
+            w="full"
+            alignItems="center"
+            justifyContent="center"
+            overflow="hidden"
+            onMouseOver={() => {
+                setState(s => ({...s, stopSliding: true}))
+            }}
+            onMouseOut={() => {
+                setState(s => ({...s, stopSliding: false}))
+            }}
+            maxHeight={{
+                base: "20em",
+                md: "30em",
+                lg: "50em"
             }}>
-              <Text
-                color="white"
-                fontSize="xs"
-                p="8px 12px"
-                pos="absolute"
-                top="0"
-                whiteSpace="nowrap">
-                {sid + 1} / {slidesCount}
-              </Text>
-              <Image 
-                className='dark-image'
-                src={slide}
-                alt="carousel image"
-                boxSize="full"
-                align={"center"}
-                fit={"cover"}
-                backgroundSize="cover"
-                
-              />
+            <Flex w="auto">
+                <Flex pos="relative" w="full" {...carouselStyle}>
+                    {props.slides.map((slide, sid) => (
+                        <Box key={`slide-${sid}`} flex="none" boxSize="full" shadow="md">
+                            <Text
+                                color="white"
+                                fontSize="xs"
+                                p="8px 12px"
+                                pos="absolute"
+                                top="0"
+                                whiteSpace="nowrap">
+                                {sid + 1} / {slidesCount}
+                            </Text>
+                            <Image
+                                className='dark-image'
+                                src={slide}
+                                alt="carousel image"
+                                boxSize="full"
+                                align={"center"}
+                                fit={"cover"}
+                                backgroundSize="cover"
 
-            </Box>
-          ))}
+                            />
+
+                        </Box>
+                    ))}
+                </Flex>
+            </Flex>
         </Flex>
-      </Flex>
-    </Flex>
-  );
+    );
 
 }
 
