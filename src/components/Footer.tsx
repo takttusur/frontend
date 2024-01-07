b40a3230caf766b0b57127426f6021c527f66fed
andreygolubkow <andreygolubkow@gmail.com>
2024-01-08 01:18:23 +0400

    Moved VK link from footer to header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, LinkBox, LinkOverlay, Text } from '@chakra-ui/react';
+import { Box, LinkBox } from '@chakra-ui/react';
 
 export default function Footer() {
   return (
@@ -9,10 +9,7 @@ export default function Footer() {
             w={'fit-content'}
             px={2}
             py={1}
-            >            
-            <LinkOverlay href='https://vk.com/takt_tusur' title='Перейти на страницу клуба в VK'>
-              <Text textDecorationLine={'underline'} fontSize={20}> Наша группа Вк </Text>
-            </LinkOverlay>
+            >
           </LinkBox>
         </Box>
     </>
--- src/components/Navigation/NavLink.tsx
+++ src/components/Navigation/NavLink.tsx
@@ -3,6 +3,7 @@ import { LinkBox, LinkOverlay, useColorModeValue } from '@chakra-ui/react';
 interface INavLinkProps {
   label: string;
   link: string;
+  isExternal?: boolean;
 }
 
 export default function NavLink(goto:INavLinkProps) {
@@ -16,7 +17,7 @@ export default function NavLink(goto:INavLinkProps) {
         textDecoration: 'none',
         bg: useColorModeValue('gray.200', 'gray.700'),
       }}>
-        <LinkOverlay href = {`/${goto.link}`} title={`Подробнее о ${goto.label}`} />
+        <LinkOverlay href = {!goto.isExternal ? `/${goto.link}` : goto.link } title={goto.label} />
         {goto.label}
     </LinkBox>
   );
