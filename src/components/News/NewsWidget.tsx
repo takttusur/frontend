10b41ee85ba2bf8e4d3d8296864cc42589eb6d22
andreygolubkow <andreygolubkow@gmail.com>
2024-01-08 02:17:55 +0400

    Created big card component for Article

--- src/components/News/NewsWidget.tsx
+++ src/components/News/NewsWidget.tsx
@@ -1,26 +1,16 @@
-import { LinkBox, LinkOverlay, Text, Box, useColorModeValue, Center } from '@chakra-ui/react';
+import {Box, Center, SimpleGrid} from '@chakra-ui/react';
+import ArticleBigCard from "./ArticleBigCard.tsx";
 
 export default function NewsWidget() {
   return (
-    <Box borderWidth='1px' borderColor='black' bg={useColorModeValue('white', 'gray.200')} h={400}>
+    <Box borderWidth='1px' borderColor='black'>
         <Center>
-            <Box w='90%' h={398} >
-              <Box position='relative' w='100%' h={398} bg='gray'>
-                <Text position='absolute' top='50%' w='100%' textAlign='center'> News </Text>
-                <LinkBox 
-                  position='absolute' bottom='0%' right='0%' w='fit-content'
-                  textDecorationLine='underline'      
-                  fontWeight={500}              
-                  pb={2}
-                  pr={2}                  
-                  >
-                  <LinkOverlay href = '/News'>
-                    <Text> Подробнее </Text>
-                  </LinkOverlay>                  
-                </LinkBox>            
-              </Box>                   
-            </Box>
-        </Center>            
+            <SimpleGrid>
+                <ArticleBigCard></ArticleBigCard>
+                <ArticleBigCard></ArticleBigCard>
+                <ArticleBigCard></ArticleBigCard>
+            </SimpleGrid>
+        </Center>     
     </Box>
   );
 }
