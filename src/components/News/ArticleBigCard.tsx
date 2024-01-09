1ea653e82bf1343cc62de3b6e342885cde69eaa0
andreygolubkow <andreygolubkow@gmail.com>
2024-01-10 01:42:00 +0400

    ArticleBigCard.tsx is connected to INewsService using react-query

--- src/components/News/ArticleBigCard.tsx
+++ src/components/News/ArticleBigCard.tsx
@@ -1,27 +1,31 @@
-import {Button, Card, CardBody, CardFooter, Image, Stack, Text} from '@chakra-ui/react';
+import { Button, Card, CardBody, CardFooter, Image, LinkOverlay, Stack, Text } from '@chakra-ui/react';
 
+interface IArticleBigCardProps {
+  imageUrl?: string;
+  text: string;
+  goUrl: string;
+}
 
-export default function ArticleBigCard() : JSX.Element {
+export default function ArticleBigCard(props : IArticleBigCardProps) : JSX.Element {
     
-    return <Card w="lg">
+  return <Card w="md">
         <CardBody>
             <Image
-                src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                alt='Green double couch with wooden legs'
+                src={props.imageUrl}
                 borderRadius='lg'
             />
-            <Stack mt='6' spacing='3'>
+            <Stack mt='3'>
                 <Text>
-                    This sofa is perfect for modern tropical spaces, baroque inspired
-                    spaces, earthy toned spaces and for people who love a chic design with a
-                    sprinkle of vintage design.
+                    {props.text}
                 </Text>
             </Stack>
         </CardBody>
         <CardFooter justify="end">
             <Button variant='solid' colorScheme='blue'>
-                Подробнее
+                <LinkOverlay href={props.goUrl}>
+                    Подробнее
+                </LinkOverlay>
             </Button>
         </CardFooter>
-    </Card>
+    </Card>;
 }
