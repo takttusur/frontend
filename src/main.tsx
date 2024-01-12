0b7cd0b302cb9bfe76ee64beb1df51dd908a60d9
andreygolubkow <andreygolubkow@gmail.com>
2024-01-12 23:41:51 +0400

    Added supporting BASE_URL for router.Added docker compose file

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ChakraProvider>
             <QueryClientProvider client={queryClient}>
-                <BrowserRouter>
+                <BrowserRouter basename={import.meta.env.BASE_URL}>
                     <App />
                 </BrowserRouter>
             </QueryClientProvider>
