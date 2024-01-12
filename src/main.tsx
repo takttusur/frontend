fea5e957167ebc8a0697c4a1770e9613b1b237c7
andreygolubkow <andreygolubkow@gmail.com>
2024-01-13 03:36:04 +0400

    Browser router was changed to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
@@ -12,9 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ChakraProvider>
             <QueryClientProvider client={queryClient}>
-                <BrowserRouter basename={import.meta.env.BASE_URL}>
+                <HashRouter>
                     <App />
-                </BrowserRouter>
+                </HashRouter>
             </QueryClientProvider>
         </ChakraProvider>
     </React.StrictMode>
